5e5c71436a96670474f6c5f5b9d2448ca84acd0d
kavidu717 <dushmanthakavidu143@gmail.com>
2026-04-25 18:41:34 +0530

    delete success page

--- villaHUB/src/Pages/success.jsx
+++
@@ -1,9 +0,0 @@
-export default function Success() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold text-green-600">
-        Payment Successful 🎉
-      </h1>
-    </div>
-  );
-}
