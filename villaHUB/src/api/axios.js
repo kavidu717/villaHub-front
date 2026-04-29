76ecf830092fce1bb53a4134006f66e4aa1adee2
kavidu717 <dushmanthakavidu143@gmail.com>
2026-04-30 02:27:21 +0530

    update backend url

--- villaHUB/src/api/axios.js
+++ villaHUB/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // my backend url
 const API = axios.create({ 
-    baseURL: "http://localhost:3000/api/v1" 
+    baseURL: "https://villahub.onrender.com/api/v1" 
 });
 
 
